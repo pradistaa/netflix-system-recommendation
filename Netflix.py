e88c3b6a9808a3d57666261d40ad8ccec98f63f9
Pradista Aprilia Winarno <87821757+pradistaa@users.noreply.github.com>
2022-04-03 19:36:55 +0700

    Add files via upload

---
+++ Netflix.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 22:47:21 2021
+
+@author: Admin
+"""
+import pandas as pd
+
+netflix_overall=pd.read_csv("netflix_titles.csv")
+netflix_overall.head()
